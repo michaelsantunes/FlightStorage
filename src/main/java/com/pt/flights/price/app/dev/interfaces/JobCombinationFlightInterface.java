ea23bd63097b5901b3ceacdc2923e830154ff457
Michael Antunes <michaelsantunes@gmail.com>
2019-05-02 15:25:34 +0100

    Update JobCombinationFlightInterface.java

--- src/main/java/com/pt/flights/price/app/dev/interfaces/JobCombinationFlightInterface.java
+++ src/main/java/com/pt/flights/price/app/dev/interfaces/JobCombinationFlightInterface.java
@@ -3,5 +3,7 @@ package com.pt.flights.price.app.dev.interfaces;
 import com.pt.flights.price.app.dev.model.ThirdTaskParameter;
 
 public interface JobCombinationFlightInterface {
+    
     void jobCombinationFlight(ThirdTaskParameter parameter);
+    
 }
