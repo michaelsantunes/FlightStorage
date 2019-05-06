e15867bb46740b1f064e9ae732316a6bf1a0fa13
Michael Antunes <michaelsantunes@gmail.com>
2019-05-06 16:58:43 +0100

    Update AirLineRepository.java

--- src/main/java/com/pt/flights/price/app/dev/repository/AirLineRepository.java
+++ src/main/java/com/pt/flights/price/app/dev/repository/AirLineRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirLineRepository extends JpaRepository<AirLine, Long> {
+    
     AirLine findAirLineByName(String name);
 }
 
